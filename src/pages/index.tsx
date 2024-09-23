130fb1be8ed3443ed41c2a10313528680fc79f82
Brandon Lambertus <lambertus.brandon@gmail.com>
2024-09-23 17:52:43 +0200

    Cookie component has been implemented in the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import CookieConsent from "@/components/Cookie";
+import { useCookies } from "react-cookie";
+
 const Homepage = () => {
+  const [cookies] = useCookies(["cookieConsent"]);
+  const [showCookieConsent, setShowCookieConsent] = useState(false);
+
+  useEffect(() => {
+    if (!cookies.cookieConsent) {
+      setShowCookieConsent(true);
+    }
+  }, [cookies]);
+
   return (
     <div>
       <h1>Welcome to the Homepage!</h1>
+      {showCookieConsent && <CookieConsent />}
     </div>
   );
 };
