09168bd6a6076fa8783b1920e6681b7d94a2a4a5
Brandon Lambertus <lambertus.brandon@gmail.com>
2024-09-23 17:39:21 +0200

    Cookie consent component is done, only needs to be implemented.

---
+++ src/components/Cookie.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { useCookies } from "react-cookie";
+
+const CookieConsent = () => {
+  const [cookies, setCookies] = useCookies(["cookieConsent"]);
+  const giveConsent = () => {
+    setCookies("cookieConsent", true, { path: "/" });
+  };
+  return (
+    <div className="cookie-container">
+      <p>
+        We use cokies to enhance your user expereince. By using our website, you
+        agree ti iyr use of cookies. {""}
+        <Link href={"/privacy-policy"}> Learn more</Link>
+      </p>
+      <button className="cookie-btn" onClick={giveConsent}>
+        Accept
+      </button>
+    </div>
+  );
+};
