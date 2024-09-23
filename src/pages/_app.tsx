6f0a586556be28db5018b05286a2ff6813fa5dad
Brandon Lambertus <lambertus.brandon@gmail.com>
2024-09-23 18:23:01 +0200

    Updated app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { CookiesProvider, useCookies } from "react-cookie";
+import { CookiesProvider } from "react-cookie";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
