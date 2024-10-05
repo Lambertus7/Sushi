f6b15b3e8544c5d78a896bf5bfbd803eab8a7e96
Brandon Lambertus <lambertus.brandon@gmail.com>
2024-10-05 15:09:05 +0200

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,4 +36,4 @@ const Navbar = () => {
 
 export default Navbar;
 
-//I am writing this so that i don't lose my streak on Github tihi
+//I am writing this so that i don't lose my streak on Github tihi.
